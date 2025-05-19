0387682b60058adbfd81573015010bfd0ca3dfc5
bhalinder <bhalindersingh63@gmail.com>
2025-05-19 14:50:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SearchBar from "./components/SearchBar";
+import CountryData from "./components/CountryData";
 
 const App = () => {
   return (
     <div className="container">
       <h1>Country Search</h1>
       <SearchBar />
+      <CountryData />
     </div>
   );
 };
